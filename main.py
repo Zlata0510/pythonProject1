edc46ff09c71150ef7f172e61e27c28389feb6d7
Zlata <65840k@gmail.com>
2023-07-11 10:43:02 +0700

    улучшенная версия бота

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio  # Работа с асинхронностью
+from tkinter import Text
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
+from aiogram.enums import ContentType
 from aiogram.filters import Command  # Фильтр для /start, /...
 from aiogram.types import Message  # Тип сообщения
 
@@ -23,6 +25,20 @@ async def start_command(message: Message):
 async def help_command(message: Message):
     await message.answer('Чем могу помочь?')
 
+#@dp.message(F.content_type == ContentType.PHOTO)
+#async def echo_photo(message: Message):
+#   await message.answer_photo(message.photo[0].file_id)
+
+# @dp.message(F.content_type == ContentType.STICKER)
+# async def echo_sticker(message: Message):
+#     await message.answer_sticker(message.sticker.file_id)
+#
+# @dp.message(F.content_type == ContentType.VOICE)
+# async def echo_voice(message: Message):
+#     await message.answer_voice(message.voice.file_id)
+@dp.message(Text(text='Ответь'))
+async def reply (message:Message):
+    await message.answer.reply('Ответил')
 
 async def main():
     try:
